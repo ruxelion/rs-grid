ba189eea566fec417f6056754b4e38ef3b047435
Benoit Podwinski <git@benoitpodwinski.com>
2026-03-24 10:32:02 +0100

    Update site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,6 +3,13 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   trailingSlash: 'never',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fr'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   vite: {
     server: {
       allowedHosts: true,
